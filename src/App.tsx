c6b0f6a8f51bb972389490100ac2970ae775da01
hmaverick47 <ouchen.hatem@hns-re2sd.dz>
2025-06-23 17:00:32 +0100

    FIX: Add basename to BrowserRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* This is the only line that changes */}
+      <BrowserRouter basename="/spiralscales/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
